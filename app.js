dac2de520af44328f659702129d77dcb1cce44e1
ngrandon-lab <ngrandon@gmail.com>
2025-08-27 22:02:03 +0300

    Actualiza app.js - Se crean las funciones agregarAmigo y sortearAmigo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+// Función que agrega los nombres de las personas e una lista
+function agregarAmigo(){
+
+}
+
+
+
+// Función de determina el nombre de la persona que será el amigo secreto  
+function sortearAmigo(){
+
+}
